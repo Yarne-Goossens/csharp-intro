dfd0d0dbbd301523f5d7deac9865f7d9176c9819
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-03-10 09:04:14 +0100

    formattingExercise

---
+++ exercises/string-interpolation/FormattingExercise/Formatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingExercise
+{
+    class Formatter
+    {
+        public static String Greet(string name){
+            return $"Hello, {name}";
+        }
+
+        public static String FormatDate(int day, int month, int year)
+        {
+            return $"{day:00}/{month:00}/{year}";
+        }
+    }
+}
